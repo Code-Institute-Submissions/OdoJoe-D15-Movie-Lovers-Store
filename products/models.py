44d086627df6f3eb0da871484b3ea3f43517b488
Joe O'Donohoe <joeodonohoe@live.com>
2022-11-05 16:43:17 +0000

    add test data fixtures and models

---
+++ products/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=150)
+    friendly_name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class Region(models.Model):
+    name = models.CharField(max_length=150)
+    friendly_name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name 
+
+class Format(models.Model):
+    name = models.CharField(max_length=150)
+    friendly_name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class Stockitem(models.Model):
+    sku = models.CharField(max_length=100)
+    name = models.CharField(max_length=150)
+    format = models.ForeignKey('Format', null=True, on_delete=models.SET_NULL)
+    is_special_edition = models.BooleanField()
+    region = models.ForeignKey('Region', null=True, on_delete=models.SET_NULL)
+    description = models.CharField(max_length=2000)
+    price = models.DecimalField(decimal_places=2)
+    genre = models.ForeignKey('Genre', null=True, on_delete=models.SET_NULL)
+    rating = models.DecimalField(decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
