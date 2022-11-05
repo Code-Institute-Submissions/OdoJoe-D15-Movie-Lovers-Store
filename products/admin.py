4f339fe961a2a255455e82b10771bd451f5f8c3e
Joe O'Donohoe <joeodonohoe@live.com>
2022-11-05 18:13:38 +0000

    customise admin and add all stockitems view

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from .models import Stockitem, Format, Region, Genre
 
-# Register your models here.
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name'
+    )
+
+    ordering = ('pk',)
+
+
+class FormatAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name'
+    )
+
+    ordering = ('pk',)
+
+
+class RegionAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name'
+    )
+
+    ordering = ('pk',)
+
+
+class StockitemAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'region',
+        'format',
+        'genre',
+        'price',
+        'is_special_edition'
+    )
+
+    ordering = ('name',)
+
+
+admin.site.register(Stockitem, StockitemAdmin)
+admin.site.register(Format, FormatAdmin)
+admin.site.register(Region, RegionAdmin)
+admin.site.register(Genre, GenreAdmin)
+
+
--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Stockitem
 
-# Create your views here.
+
+def all_stockitems(request):
+    """
+    show all stock items
+    """
+    stockitems = Stockitem.objects.all()
+
+    context = {
+        'stockitems': stockitems,
+    }
+
+    return render(request, 'products/stockitems.html', context)
