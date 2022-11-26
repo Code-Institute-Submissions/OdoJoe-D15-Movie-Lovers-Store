689748f13ffd58bedde8a72d65de057926b5bf8a
Joe O'Donohoe <joeodonohoe@live.com>
2022-11-26 15:15:05 +0000

    set up auto deployment to heroku

--- d15_movie_lovers_store/settings.py
+++ d15_movie_lovers_store/settings.py
@@ -22,10 +22,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5rh4g(406hg#i4p-mqm7i%&@qngmtjxa!+o#2%vsh6*$^ecmei'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['d15-movie-lovers-store.herokuapp.com', 'localhost']
 
