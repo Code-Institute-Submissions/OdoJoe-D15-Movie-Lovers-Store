b16a2882b069ad61492d6e57331202f143399e8a
Joe O'Donohoe <joeodonohoe@live.com>
2022-12-05 21:37:23 +0000

    start newsletter model and view

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('<user_id>', views.add_to_newsletter, name='add_to_newsletter'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.contrib.auth.models import User
+from .models import Subscriber
 
 def index(request):
     """
     Index page view
     """
     return render(request, 'home/index.html')
+
+
+def add_to_newsletter(request, user_id):
+    """
+    View to add subscriber
+    """
+    user = User.objects.get(id=user_id)
+
+    subscriber = Subscriber()
+    subscriber.username = user.username
+    subscriber.email = user.email
+    subscriber.save()
+
+    subscribers = Subscriber.objects.all()
+    context = {
+        'subscribers': subscribers,
+    }
+    return render(request, 'home/index.html', context)
