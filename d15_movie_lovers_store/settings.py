06b8694441ad4d730030ce4957753df5779106e9
Joe O'Donohoe <joeodonohoe@live.com>
2022-12-16 12:10:54 +0000

    Heroku test Debug False

--- d15_movie_lovers_store/settings.py
+++ d15_movie_lovers_store/settings.py
@@ -25,7 +25,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  # 'DEVELOPMENT' in os.environ
+DEBUG = False  # 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['d15-movie-lovers-store.herokuapp.com', 'localhost']
 
